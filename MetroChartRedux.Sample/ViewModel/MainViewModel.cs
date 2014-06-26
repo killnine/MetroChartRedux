e42c7bb751b90fd6059cc9ce691aa1735d3f836c
killnine <derek.heiser@gmail.com>
2014-06-26 09:55:48 -0500

    Modifying Styles
    Updated Styles to show FPM instead of %

--- MetroChartRedux.Sample/ViewModel/MainViewModel.cs
+++ MetroChartRedux.Sample/ViewModel/MainViewModel.cs
@@ -5,10 +5,14 @@ namespace MetroChartRedux.Sample.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
-        public ObservableCollection<SeriesClass> Speed { get; private set; } 
+        public ObservableCollection<SeriesClass> Speed { get; private set; }
+
+        public int ImpressionsPerHour { get; set; }
 
         public MainViewModel()
         {
+            ImpressionsPerHour = 24000;
+
             Speed = new ObservableCollection<SeriesClass>
             {
                 new SeriesClass { Category = "Feet Per Minute", Number = 75}
