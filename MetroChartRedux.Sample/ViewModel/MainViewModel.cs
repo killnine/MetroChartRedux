3ab1cdd9e2fdf1ab77997b5817e7007b5cb79b83
killnine <derek.heiser@gmail.com>
2014-06-26 09:23:31 -0500

    Build Fixed
    With help from @davepermen.net, got build fixed and basic Gauge to
    display.

--- MetroChartRedux.Sample/ViewModel/MainViewModel.cs
+++ MetroChartRedux.Sample/ViewModel/MainViewModel.cs
@@ -1,34 +1,24 @@
+using System.Collections.ObjectModel;
 using GalaSoft.MvvmLight;
 
 namespace MetroChartRedux.Sample.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
     public class MainViewModel : ViewModelBase
     {
-        /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
+        public ObservableCollection<SeriesClass> Speed { get; private set; } 
+
         public MainViewModel()
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+            Speed = new ObservableCollection<SeriesClass>
+            {
+                new SeriesClass { Category = "Feet Per Minute", Number = 75}
+            };
         }
+    }
+
+    public class SeriesClass
+    {
+        public string Category { get; set; }
+        public int Number { get; set; }
     }
 }
